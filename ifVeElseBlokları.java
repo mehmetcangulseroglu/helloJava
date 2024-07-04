581eacd8125b9c673e3c2a36f4e14526e008c05f
Mehmet Can Gülseroğlu <mcagul99@gmail.com>
2024-07-04 22:23:54 +0300

    şart blokları işlendi

---
+++ ifVeElseBlokları.java
@@ -0,0 +1,20 @@
+public class ifVeElseBlokları {
+    public static void main(String args[]){
+        int sayi1=50;
+        int sayi2=51;
+        if(sayi1>sayi2){
+            System.out.println(sayi1+" büyüktür "+ sayi2 +" den");
+        }
+        else if(sayi1<sayi2){
+            System.out.println(sayi1+" küçüktür "+ sayi2+" den");
+        }
+        else{
+            System.out.println(sayi1+" eşittir "+ sayi2+" ye");
+        }
+
+
+    }
+
+
+    }
+
