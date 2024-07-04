a3c53a6f0b68dedcc864d8bc3e5a39a5a27b776e
mehmetcangulseroglu <mcagul99@gmail.com>
2024-07-04 14:31:07 +0300

    degiskenler ve veri tipleri islendi

---
+++ degiskenlerVeVeritipleri.java
@@ -0,0 +1,29 @@
+
+public class degiskenlerVeVeritipleri {
+    public static void main(String[] args) {
+        int a = 25;
+        byte abc = 100;
+        System.out.println(abc);
+        short shr = 1000;
+        System.out.println("short degeri : "+shr);
+        long lng =1000000;
+        System.out.println(lng);
+
+        float sayi=34.5f;//float sayılarda değer sonuna f veya F koyulmalı
+        System.out.println(sayi);
+        double sayi2=3.14;
+        System.out.println(sayi2);
+
+        char karakter = 'a';
+        System.out.println(karakter);
+        char karakter2 = 117;//117 u harfinin ascıı karşılığıdır dolayısıyla çıktı olarak u harfini verir.
+        System.out.println(karakter2);
+
+        boolean dogruMu=true;//boolean true veya false değerlerini alır sadece
+        System.out.println(dogruMu);
+
+        String isim ="mehmet can gülseroğlu";
+        System.out.println(isim);
+        System.out.println("15"+20);
+        }
+    }
