635df9edfe5e4bab0ce474be51bd57692ed5bbb1
mehmetcangulseroglu <mcagul99@gmail.com>
2024-07-17 02:34:33 +0300

    sınıflar işlendi

---
+++ OOP1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //örneksınıf.java sınıfından türetilen ilk obje
+        örneksınıf makine1=new örneksınıf();
+        makine1.sayi1=12;
+        makine1.sayi2=32;
+        System.out.println(makine1.sayi1);
+
+        //örnekSınıf.java sınıfından türetilen ikinci obje
+        örneksınıf makine2=new örneksınıf();
+        makine2.sayi1=65;
+        makine2.sayi2=31;
+        System.out.println(makine2.sayi2);
+    }
+}
---
+++ OOP1/src/örneksınıf.java
@@ -0,0 +1,5 @@
+public class örneksınıf {
+    public int sayi1=10;
+    public int sayi2=20;
+
+}
